ae6fca390fbb3358cf13de4d1c1f014c157fddc9
Saleh Mehdikhani <saleh.mehdikhani@unikie.com>
2024-04-16 11:37:07 +0300

    Handle all errors at writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -24,9 +24,20 @@ int main(int argc, char *argv[]) {
     }
 
     // write to the file
-    fprintf(fileh, "%s", text_str);
+    int num_written = fprintf(fileh, "%s", text_str);
+    if (num_written < 0) {
+        syslog(LOG_ERR, "Error to write to the file: %s", strerror(fopen_err));
+        fclose(fileh);
+        exit(1);
+    }
     syslog(LOG_DEBUG, "Writing %s to %s", text_str, file_path);
 
     // close the file
-    fclose(fileh);
+    int close_err = fclose(fileh);
+    if (close_err != 0) {
+        syslog(LOG_ERR, "Error closing the file: %s", strerror(fopen_err));
+        exit(1);
+    }
+
+    return 0;
 }
